3362fd59fb37a6bd53dc85110969257df78212aa
Michal <marszalmeins@gmail.com>
2022-04-27 16:49:51 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace iSMAControlli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This works only when I know that someNumber is always a Int number
+            Console.WriteLine(IsNumberDevideByTwo(Convert.ToInt32(Console.ReadLine())));
+
+            Console.ReadKey();
+        }
+
+        public static bool IsNumberDevideByTwo(int someNumber)
+        {
+            if(someNumber % 2 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
